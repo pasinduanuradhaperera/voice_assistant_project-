8545994f6bb0c8d75c872b575b78a183879ea222
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2023-06-23 14:10:20 +0530

    check packages

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import speech_recognition as sr
 
+import time
+
 listener = sr.Recognizer()
+try:
+    with sr.Microphone() as source:
+        print('Listening...')
+        time.sleep(1)
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(command)
+
+except:
+    pass
+
